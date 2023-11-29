804477ba29cbf21383ad671667dcfe9d8ee63950
alghiffari10 <raihanalghiffary28@gmail.com>
2023-11-29 13:25:10 +0700

    menambahkan news dan kegiatan page dan melakukan responsive design

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,20 +1,24 @@
 import icNews from '../assets/ic-news.png';
 import icCalendar from '../assets/ic-calendar.png';
+import bannerNews from '../assets/banner-news.jpg';
 
 const News = ({ name, type, calendar }) => {
   return (
     <div className="container-news">
       <div className="news-list">
         <div className="news">
-          <h5 className="header-news">{name}</h5>
-          <div className="ic-news">
-            <img className="icon-news" src={icNews} alt="icon news" />
-            <span className="news-text">{type}</span>
+          <div className="text-overlay">
+            <h5 className="header-news">{name}</h5>
+            <div className="ic-news">
+              <img className="icon-news" src={icNews} alt="icon news" />
+              <span className="news-text">{type}</span>
+            </div>
+            <div className="ic-calendar">
+              <img className="icon-calendar" src={icCalendar} alt="icon calendar" />
+              <span className="calender-text">{calendar}</span>
+            </div>
           </div>
-          <div className="ic-calendar">
-            <img className="icon-calendar" src={icCalendar} alt="icon calendar" />
-            <span className="calender-text">{calendar}</span>
-          </div>
+          <img src={bannerNews} alt="" className="news-image" />
         </div>
       </div>
     </div>
