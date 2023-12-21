3aba021f092f8377d2ffb3f93fbaca7d020fc3ef
rajastra <rajasaputeraxii.ipa5@gmail.com>
2023-12-21 21:32:08 +0700

    update data banner

--- src/data.js
+++ src/data.js
@@ -1,92 +1,92 @@
-import murid from './assets/murid.png';
-import guru from './assets/guru.png';
-import tenaga_pendidik from './assets/tenaga pendidik.png';
-import kelas from './assets/kelas.png';
-import kepalaSekolah from './assets/kepala-sekolah.png';
-import landingProfile from './assets/landing profile.jpg';
-import Wifi from './assets/wifi.png';
-import labComputer from './assets/lab-computer.png';
-import labIPA from './assets/laboratorium-ipa.png';
-import perpustakaan from './assets/perpustakaan.png';
-import lapanganBasket from './assets/lapangan-basket.png';
-import lapanganVolley from './assets/lapangan-volley.png';
+import murid from "./assets/murid.png";
+import guru from "./assets/guru.png";
+import tenaga_pendidik from "./assets/tenaga pendidik.png";
+import kelas from "./assets/kelas.png";
+import kepalaSekolah from "./assets/kepala-sekolah.png";
+import landingProfile from "./assets/landing profile.jpg";
+import Wifi from "./assets/wifi.png";
+import labComputer from "./assets/lab-computer.png";
+import labIPA from "./assets/laboratorium-ipa.png";
+import perpustakaan from "./assets/perpustakaan.png";
+import lapanganBasket from "./assets/lapangan-basket.png";
+import lapanganVolley from "./assets/lapangan-volley.png";
 
 export const info_data = [
   {
     id: 1,
     image: murid,
-    count: '465',
-    type: 'Murid',
+    count: "491",
+    type: "Murid",
   },
   {
     id: 2,
     image: guru,
-    count: '465',
-    type: 'Guru',
+    count: "33",
+    type: "Guru",
   },
   {
     id: 3,
     image: tenaga_pendidik,
-    count: '465',
-    type: 'Tenaga Pendidik',
+    count: "9",
+    type: "Tenaga Pendidik",
   },
   {
     id: 4,
     image: kelas,
-    count: '465',
-    type: 'Kelas',
+    count: "17",
+    type: "Kelas",
   },
 ];
 
 export const news_data = [
   {
     id: 1,
-    name: '3 siswa dengan nilai ujian terbaik',
-    type: 'news',
-    calendar: '2023-05-10',
+    name: "3 siswa dengan nilai ujian terbaik",
+    type: "news",
+    calendar: "2023-05-10",
   },
   {
     id: 2,
-    name: '3 siswa dengan nilai ujian terbaik',
-    type: 'news',
-    calendar: '2023-05-40',
+    name: "3 siswa dengan nilai ujian terbaik",
+    type: "news",
+    calendar: "2023-05-40",
   },
   {
     id: 3,
-    name: 'siswa ini memenangkan nobel',
-    type: 'news',
-    calendar: '2023-02-20',
+    name: "siswa ini memenangkan nobel",
+    type: "news",
+    calendar: "2023-02-20",
   },
 ];
 export const kegiatan_data = [
   {
     id: 1,
-    name: '3 siswa dengan nilai ujian terbaik',
-    type: 'kegiatan',
-    calendar: '2023-05-10',
+    name: "3 siswa dengan nilai ujian terbaik",
+    type: "kegiatan",
+    calendar: "2023-05-10",
   },
   {
     id: 2,
-    name: '3 siswa dengan nilai ujian terbaik',
-    type: 'kegiatan',
-    calendar: '2023-05-40',
+    name: "3 siswa dengan nilai ujian terbaik",
+    type: "kegiatan",
+    calendar: "2023-05-40",
   },
   {
     id: 3,
-    name: 'siswa ini memenangkan nobel',
-    type: 'kegiatan',
-    calendar: '2023-02-20',
+    name: "siswa ini memenangkan nobel",
+    type: "kegiatan",
+    calendar: "2023-02-20",
   },
 ];
 
 export const sambutan_data = [
   {
     id: 1,
-    header: 'Sambutan',
+    header: "Sambutan",
     image: kepalaSekolah,
-    name: 'Drs. H. Sasmadi',
+    name: "Drs. H. Sasmadi",
     deskripsi:
-      'Assalamualaikum Warahmatullohi Wabarokatuh Pertama-tama yang patut kita ucapkan adalah bersyukur dihadapan Allah SWT, Tuhan Yang Mahakuasa atas karunia yang telah diberikan kepada kita semua, sehingga sampai saat ini masih\
+      "Assalamualaikum Warahmatullohi Wabarokatuh Pertama-tama yang patut kita ucapkan adalah bersyukur dihadapan Allah SWT, Tuhan Yang Mahakuasa atas karunia yang telah diberikan kepada kita semua, sehingga sampai saat ini masih\
             bisamelaksanakan tugas-tugas mulia yang Insya Allah dilakukan dengan penuh ketulusan, keikhlasan dan dedikasi. Pada kesempatan ini kami menyampaikan ucapan terima kasih kepada semua stakeholders SMAN1 Pulaupanggung,\
             KabupatenTanggamus Provinsi Lampung yang telah berbagi pemikiran dan informasi untuk kemajuan mutu sekolah. SMAN1 Pulaupanggung merupakan salah satu wadah bagi putra putri Indonesia dalam mengisi dan membentuk pikiran-pikirannya\
             agarmenjadi manusia yang berilmu sekaligus memiliki ahlak mulia. Pendidikan merupakan kunci bagi pengembangan diri pribadi anak-anak kita, sedangkan pendidikan di lingkungan keluarga merupakan bagian yang tidak terpisahkan\
@@ -94,65 +94,65 @@ export const sambutan_data = [
             hidupkeluarga. Sejalan dengan globlisasi yang tidak terbendung lagi saat ini, kemajuan teknologi informasi memegang peran vital. Kecanggihan teknologi dapat menjalin komunikasi yang baik antara sekolah, masyarakat dan\
             pemerintah. Olehsebab itu SMAN1 Pulaupanggung membuka diri melalui website sekolah ini sebagai jawaban tuntutan kemajuan teknologi. Website ini diharapkan dapat menjalin komunikasi untuk dapat menyampaikan dan mensosialisasikan\
             program sertakebijakan sekolah sesuai tuntutan masa kini. Hal ini juga dalam rangka mewujudkan sekolah yang bersih , sehat, asri, nyaman, aman, toleran, transparan, akuntabel dan berintegritas. Akhirnya semoga website ini dapat\
-            diterima danmarilah kita melaksanakan tugas mulia ini dengan sungguh-sungguh, seraya berdoa semoga Allah Subhanahu Wa Taala melindungi kita semua, Amin. Wassalamualaikaum Warrahmatulohi Wabarokatuh Drs. H.Sasmadi',
+            diterima danmarilah kita melaksanakan tugas mulia ini dengan sungguh-sungguh, seraya berdoa semoga Allah Subhanahu Wa Taala melindungi kita semua, Amin. Wassalamualaikaum Warrahmatulohi Wabarokatuh Drs. H.Sasmadi",
   },
 ];
 
 export const sejarah_data = [
   {
     id: 1,
-    header: 'Sejarah',
+    header: "Sejarah",
     image: landingProfile,
     // name: 'Drs. H. Sasmadi',
     deskripsi:
-      'Sejarah Singkat SMAN 1 Pulau Panggung Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque nibh odio, ac congue enim ultricies in. Curabitur non faucibus nibh. Vivamus ac risus a mauris porta cursus et at ligula. Vestibulum egestas mi at velit mollis, in placerat risus gravida. In venenatis tortor ante, non fringilla sem ultricies id. Vestibulum in imperdiet leo. Nulla id eleifend risus, non ornare odio.',
+      "Sejarah Singkat SMAN 1 Pulau Panggung Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque nibh odio, ac congue enim ultricies in. Curabitur non faucibus nibh. Vivamus ac risus a mauris porta cursus et at ligula. Vestibulum egestas mi at velit mollis, in placerat risus gravida. In venenatis tortor ante, non fringilla sem ultricies id. Vestibulum in imperdiet leo. Nulla id eleifend risus, non ornare odio.",
   },
 ];
 
 export const misi_data = [
   {
     id: 1,
-    misi: 'Mewujudkan kegiatan belajar mengajar dan bimbingan secara efektif',
+    misi: "Mewujudkan kegiatan belajar mengajar dan bimbingan secara efektif",
   },
   {
     id: 2,
-    misi: 'Membangkitkan semangat warga sekolah untuk mencapai kualitas yang lebih baik',
+    misi: "Membangkitkan semangat warga sekolah untuk mencapai kualitas yang lebih baik",
   },
   {
     id: 3,
-    misi: 'Mendorong dan membantu siswa untuk mengenali potensi dirinya sehingga dapat dikembangkan secara positif',
+    misi: "Mendorong dan membantu siswa untuk mengenali potensi dirinya sehingga dapat dikembangkan secara positif",
   },
   {
     id: 4,
-    misi: 'Menanamkan sifat kreatif dan inovatif baik akademis maupun non akademis bagi warga sekolah',
+    misi: "Menanamkan sifat kreatif dan inovatif baik akademis maupun non akademis bagi warga sekolah",
   },
   {
     id: 5,
-    misi: 'Menumbuhkan penghayatan keagamaan yang akan menuntun sikap dan prilaku yang sesuai dengan norma agama dan social',
+    misi: "Menumbuhkan penghayatan keagamaan yang akan menuntun sikap dan prilaku yang sesuai dengan norma agama dan social",
   },
   {
     id: 6,
-    misi: 'Mewujudkan siswa mampu berapresiasi dan berkreasi dalam bidang seni dan teknologi',
+    misi: "Mewujudkan siswa mampu berapresiasi dan berkreasi dalam bidang seni dan teknologi",
   },
   {
     id: 7,
-    misi: 'Mewujudkan sarana prasarana sebagai pendukung kegiatan belajar mengajar secara efektif',
+    misi: "Mewujudkan sarana prasarana sebagai pendukung kegiatan belajar mengajar secara efektif",
   },
   {
     id: 8,
-    misi: 'Meningkatkan disiplin bagi warga sekolah',
+    misi: "Meningkatkan disiplin bagi warga sekolah",
   },
   {
     id: 9,
-    misi: 'Mewujudkan lingkungan sekolah yang aman, nyaman',
+    misi: "Mewujudkan lingkungan sekolah yang aman, nyaman",
   },
   {
     id: 10,
-    misi: 'Mewujudkan sekolah bersih, sehat serta peduli lingkungan menuju Sekolah Adiwiyata',
+    misi: "Mewujudkan sekolah bersih, sehat serta peduli lingkungan menuju Sekolah Adiwiyata",
   },
   {
     id: 11,
-    misi: 'Mewujudkan kerjasama dengan unsur masyarakat, komite sekolah maupun instansi yang lain',
+    misi: "Mewujudkan kerjasama dengan unsur masyarakat, komite sekolah maupun instansi yang lain",
   },
 ];
 
@@ -160,31 +160,31 @@ export const fasilitas_data = [
   {
     id: 1,
     image: Wifi,
-    fasilitas: 'Wifi',
+    fasilitas: "Wifi",
   },
   {
     id: 2,
     image: labComputer,
-    fasilitas: 'Lab Computer',
+    fasilitas: "Lab Computer",
   },
   {
     id: 3,
     image: labIPA,
-    fasilitas: 'Lab IPA',
+    fasilitas: "Lab IPA",
   },
   {
     id: 4,
     image: perpustakaan,
-    fasilitas: 'Perpustakaan',
+    fasilitas: "Perpustakaan",
   },
   {
     id: 5,
     image: lapanganBasket,
-    fasilitas: 'Lapangan Basket',
+    fasilitas: "Lapangan Basket",
   },
   {
     id: 6,
     image: lapanganVolley,
-    fasilitas: 'Lapangan Voli',
+    fasilitas: "Lapangan Voli",
   },
 ];
